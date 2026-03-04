1b610a83d1619c161e1b92480df94831732de58e
Sully <sharrer619@gmail.com>
2026-03-03 17:51:24 -0700

    Add positionless heatmap

--- pipelines/maintenance/create_digit_heatmap.py
+++ pipelines/maintenance/create_digit_heatmap.py
@@ -2,6 +2,11 @@ import pandas as pd
 import numpy as np
 import seaborn as sns
 import matplotlib.pyplot as plt
+import os
+
+
+def clear_screen():
+    os.system('cls' if os.name == 'nt' else 'clear')
 
 def make_position_heatmap(csv_path, output_img='digit_heatmap.png'):
     df = pd.read_csv(csv_path)
@@ -33,5 +38,53 @@ def make_position_heatmap(csv_path, output_img='digit_heatmap.png'):
     plt.savefig(output_img)
     print(f"Heatmap saved to {output_img}")
 
+def make_positionless_heatmap(csv_path, output_img='positionless_digit_heatmap.png'):
+    df = pd.read_csv(csv_path)
+    
+    counts = np.zeros((10, 1))
+    
+    for weight in df['weight']:
+        w_str = f"{float(weight):.3f}"
+        for c in w_str:
+            if c.isdigit():
+                counts[int(c),0] += 1
+            
+    # Convert to dataframe for better plotting labels
+    heatmap_df = pd.DataFrame(
+        counts, 
+        index=[str(i) for i in range(10)],
+        columns=['count']
+    )
+    
+    plt.figure(figsize=(6, 8))
+    sns.heatmap(heatmap_df, annot=True, fmt='g', cmap='YlGnBu')
+    plt.title('Digit Frequencey Distribution (Positionless)')
+    plt.xlabel('Total count')
+    plt.ylabel('Digit value')
+    plt.savefig(output_img)
+    print(f"Heatmap saved to {output_img}")
+
 if __name__ == "__main__":
-    make_position_heatmap('data/all_data.csv')
+    while True:
+        clear_screen()
+        print("Which of the following would you like to do?")
+        print("1. Create a position-specific heatmap")
+        print("2. Create a position agnostic heatmap")
+        print("3. Create both heatmaps\n")
+
+        choice = input("Select an option: ").strip()
+
+        if choice == '1':
+            make_position_heatmap('data/all_data.csv')
+            break
+        elif choice == '2':
+            make_positionless_heatmap('data/all_data.csv')
+            break
+        elif choice == '3':
+            make_position_heatmap('data/all_data.csv')
+            make_positionless_heatmap('data/all_data.csv')
+            break
+        else:
+            print("Invalid choice. Please try again.")
+            input("Press Enter to continue...")
+            continue
